4658b270a8ce72823a0c4b73bfdde72711bc5d25
Javi H. Gil <javihgil@softspring.es>
2024-03-05 11:40:47 +0100

    Update php-cs-fixer

--- src/Exception/InvalidFilterFormException.php
+++ src/Exception/InvalidFilterFormException.php
@@ -6,7 +6,7 @@ use Softspring\Component\DoctrineQueryFilters\FilterFormInterface;
 
 class InvalidFilterFormException extends \Exception
 {
-    public function __construct($message = '', $code = 0, \Throwable $previous = null)
+    public function __construct($message = '', $code = 0, ?\Throwable $previous = null)
     {
         $message = $message ?: sprintf('$filterForm type must implement %s', FilterFormInterface::class);
         parent::__construct($message, $code, $previous);
